489a3ab5b4b378833a21ae112ed4c62a339c5608
AndrewPaparella <paparella.andrew@gmail.com>
2021-07-23 12:13:09 -0700

    completes part 4

--- server.js
+++ server.js
@@ -1,28 +1,31 @@
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+require("dotenv").config();
+require("./config/database");
 
-require('dotenv').config();
-require('./config/database');
-
+const puppiesRouter = require("./routes/api/users");
 
 const app = express();
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
+// Configure both serve-favicon & static middleware
+// to serve from the production 'build' folder
+app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, 'build')));
-
-
-
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
+app.use("/api/users", puppiesRouter);
+// Configure to use port 3001 instead of 3000 during
+// development to avoid collision with React's dev server
 const port = process.env.PORT || 3001;
-
-app.listen(port, function() {
-  console.log(`Express app running on port ${port}`)
+app.listen(port, function () {
+  console.log(`Express app running on port ${port}`);
+});
+app.use(express.static(path.join(__dirname, "build")));
+// Put API routes here, before the "catch all" route
+// The following "catch all" route (note the *) is necessary
+// to return the index.html on all non-AJAX requests
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
